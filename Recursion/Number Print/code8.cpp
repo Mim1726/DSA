dd144bf3c29c96d65850b6282b5cfa4ae61987e2
Suraya Jannat Mim <150699804+Mim1726@users.noreply.github.com>
2024-12-14 13:26:32 +0600

    Create code8.cpp

---
+++ Recursion/Number Print/code8.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+void func(int x){
+    if(x>=10)
+        return;
+    func(x+1);
+    func(x+2);
+    cout << x << endl;
+}
+int main(){
+    int x;
+    cin >> x;
+    func(x);
+    /* input: f(6)
+    f(7)
+    f(8)
+    f(9)
+    f(10)->
+    f(9)
+    f(11)->f(9) print 9
+    f(8)
+    f(10)->f(8) print 8
+    f(7)---------
+    f(9) f(10)->f(9)
+    f(11)->f(9) print 9
+    f(7)        print 7
+    f(6)---------
+    f(8)
+    f(9) f(10) f(9)
+    f(11)->f(9) print 9
+    f(8) f(10)
+    f(8)        print 8
+    f(6)        print 6
+    */
+    return 0;
+}
