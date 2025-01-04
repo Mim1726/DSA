4d5b75876b987295822811559dd463389c70927c
Suraya Jannat Mim <150699804+Mim1726@users.noreply.github.com>
2025-01-04 19:36:43 +0600

    Update and rename Tree/inorder2.cpp to Tree/Traversal/preorder.cpp

---
+++ Tree/Traversal/preorder.cpp
@@ -0,0 +1,68 @@
+#include<bits/stdc++.h>
+using namespace std;
+struct tree{
+    struct node{
+        int val;
+        node* left;
+        node* right;
+        node* parent;
+    };
+    node* root=NULL;
+
+    void insertion(int x){
+        node* a=new node();
+        a->val=x;
+        a->left=NULL;
+        a->right=NULL;
+        a->parent=NULL;
+
+        if(!root){
+            root=a;
+            return;
+        }
+
+        queue<node*> q;
+        q.push(root);
+
+        while(!q.empty()){
+            node* curr=q.front();
+            q.pop();
+
+            if(!curr->left){
+                curr->left=a;
+                a->parent=curr;
+                return;
+            }
+            else{
+                q.push(curr->left);
+            }
+
+            if(!curr->right){
+                curr->right=a;
+                a->parent=curr;
+                return;
+            }
+            else{
+                q.push(curr->right);
+            }
+        }
+    }
+    void preorder(node* p){
+        if(!p) return;
+        cout << p->val << " ";
+        preorder(p->left);
+        preorder(p->right);
+    }
+};
+int main(){
+    tree my_tree;
+    int n;
+    cin >> n;
+    for(int i=0;i<n;i++){
+        int x;
+        cin >> x;
+        my_tree.insertion(x);
+    }
+    my_tree.preorder(my_tree.root);
+    return 0;
+}
