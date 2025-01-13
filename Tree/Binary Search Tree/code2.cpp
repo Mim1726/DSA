e780d7e76303e0ece2b330b52b060cd9d575c40a
Suraya Jannat Mim <150699804+Mim1726@users.noreply.github.com>
2025-01-13 10:01:41 +0600

    Create code2.cpp

---
+++ Tree/Binary Search Tree/code2.cpp
@@ -0,0 +1,93 @@
+#include<bits/stdc++.h>
+using namespace std;
+struct node{
+    int val;
+    node* left;
+    node* right;
+    node(int x){
+        val=x;
+        left=nullptr;
+        right=nullptr;
+    }
+};
+node* insert(node* root,int x){
+    if(root == nullptr)
+        return new node(x);
+    if(x < root->val){
+        root->left=insert(root->left,x);
+    }
+    else if(x > root->val){
+        root->right=insert(root->right,x);
+    }
+}
+void inorder(node* p){
+    if(p == nullptr) return;
+    inorder(p->left);
+    cout << p->val << " ";
+    inorder(p->right);
+}
+bool find(node* p,int x){
+    if(p == nullptr) return false;
+
+    if(p->val == x) return true;
+
+    if(x < p->val){
+        return find(p->left,x);
+    }
+    else{
+        return find(p->right,x);
+    }
+}
+node* findmin(node* root){
+    while(root->left != nullptr){
+        root=root->left;
+    }
+    return root;
+}
+node* delete_node(node* root,int x){
+    if(root == nullptr) return root;
+
+    if(x < root->val){
+        root->left=delete_node(root->left,x);
+    }
+
+    else if(x > root->val){
+        root->right=delete_node(root->right,x);
+    }
+
+    else{
+        if(root->left == nullptr){
+            node* tmp=root->right;
+            delete root;
+            return tmp;
+        }
+        else if(root->right == nullptr){
+            node* tmp=root->left;
+            delete root;
+            return tmp;
+        }
+        node* tmp=findmin(root->right);
+        root->val=tmp->val;
+        root->right=delete_node(root->right,tmp->val);
+    }
+    return root;
+}
+int main(){
+    node* root=nullptr;
+    int n;
+    cin >> n;
+    for(int i=0;i<n;i++){
+        int x;
+        cin >> x;
+        root=insert(root,x);
+    }
+    int v,q;
+    cin >> v >> q;
+    cout << find(root,v) << endl;
+    cout << find(root,q) << endl;
+
+    root=delete_node(root,v);
+    cout << find(root,v) << endl;
+
+    return 0;
+}
