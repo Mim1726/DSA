1043b9ec9000f1ab96fcf517e6d533b51a4cf9c5
Suraya Jannat Mim <150699804+Mim1726@users.noreply.github.com>
2025-01-25 17:36:23 +0600

    Create template.cpp

---
+++ Disjoint Set Union/template.cpp
@@ -0,0 +1,68 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int parent[1000];
+int _size[1000];
+int _rank[1000];
+
+void make_set(int v){
+    parent[v] = v;
+    _size[v] = 1;
+    _rank[v]= 0 ;
+}
+
+int find_set(int v){
+    if(v == parent[v])
+        return v;
+    return find_set(parent[v]);
+}
+
+void union_naive(int a, int b){
+    a = find_set(a);
+    b = find_set(b);
+    if(a==b) return;
+    parent[a] = b;
+}
+
+void union_by_size(int a, int b){
+    a = find_set(a);
+    b = find_set(b);
+    if(a==b) return;
+    if(_size[a] > _size[b]){
+        parent[b] = a;
+        _size[a] += _size[b];
+    }
+    else{
+        parent[a] = b;
+        _size[b] += _size[a];
+    }
+}
+
+void union_by_rank(int a, int b){
+    a = find_set(a);
+    b = find_set(b);
+    if(a==b) return;
+    if(_rank[a] > _rank[b]){
+        parent[b] = a;
+    }
+    else if(_rank[b] > _rank[a]){
+        parent[a] = b;
+    }
+    else{
+        parent[b] = a;
+        _rank[a]++;
+    }
+}
+
+int main(){
+    for(int i=0;i<1000;i++){
+        parent[i] = -1;
+    }
+
+    for(int i=1;i<=10;i++){
+        make_set(i);
+    }
+
+
+    return 0;
+}
