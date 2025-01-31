858f252eb7b9d895ba69334f0f8b7c0d9cda579b
Suraya Jannat Mim <150699804+Mim1726@users.noreply.github.com>
2025-01-31 11:33:18 +0600

    Update template.cpp

--- Disjoint Set Union/template.cpp
+++ Disjoint Set Union/template.cpp
@@ -8,7 +8,7 @@ int _rank[1000];
 void make_set(int v){
     parent[v] = v;
     _size[v] = 1;
-    _rank[v]= 0 ;
+    _rank[v] = 0;
 }
 
 int find_set(int v){
@@ -17,17 +17,29 @@ int find_set(int v){
     return find_set(parent[v]);
 }
 
-void union_naive(int a, int b){
+/*
+using loop
+
+int find_set(int v){
+    int a = v;
+    while(a != parent[a]){
+        a = parent[a];
+    }
+    return a;
+}
+*/
+
+void union_naive(int a,int b){
     a = find_set(a);
     b = find_set(b);
-    if(a==b) return;
+    if(a == b) return;
     parent[a] = b;
 }
 
-void union_by_size(int a, int b){
+void union_by_size(int a,int b){
     a = find_set(a);
     b = find_set(b);
-    if(a==b) return;
+    if(a == b) return;
     if(_size[a] > _size[b]){
         parent[b] = a;
         _size[a] += _size[b];
@@ -38,10 +50,10 @@ void union_by_size(int a, int b){
     }
 }
 
-void union_by_rank(int a, int b){
+void union_by_rank(int a,int b){
     a = find_set(a);
     b = find_set(b);
-    if(a==b) return;
+    if(a == b) return;
     if(_rank[a] > _rank[b]){
         parent[b] = a;
     }
@@ -49,20 +61,23 @@ void union_by_rank(int a, int b){
         parent[a] = b;
     }
     else{
-        parent[b] = a;
-        _rank[a]++;
+        if(_size[a] > _size[b]){
+            parent[b] = a;
+            _rank[a]++;
+        }
+        else{
+            parent[a] = b;
+            _rank[b]++;
+        }
     }
 }
 
 int main(){
+
     for(int i=0;i<1000;i++){
         parent[i] = -1;
     }
-
     for(int i=1;i<=10;i++){
         make_set(i);
     }
-
-
-    return 0;
 }
