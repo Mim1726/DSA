b9f2f80b137f556de271bda58675eec0958b53dc
Suraya Jannat Mim <150699804+Mim1726@users.noreply.github.com>
2025-01-31 11:58:05 +0600

    Update bfs.cpp

--- Graph/bfs.cpp
+++ Graph/bfs.cpp
@@ -5,37 +5,37 @@ void bfs(vector<vector<int>>& adj,int x){
     queue<int> q;
     vector<bool> visited(adj.size(),false);
 
-    visited[x]=true;
+    visited[x] = true;
     q.push(x);
 
     while(!q.empty()){
-        int curr=q.front();
+        int curr = q.front();
         q.pop();
         cout << curr << " ";
 
-        for(int x: adj[curr]){
-            if(!visited[x]){
-                visited[x]=true;
-                q.push(x);
+        for(int v: adj[curr]){
+            if(!visited[v]){
+                visited[v] = true;
+                q.push(v);
             }
         }
     }
 }
 
-void addEdge(vector<vector<int>>& adj,int u,int v){
+void add_edge(vector<vector<int>>& adj,int u,int v){
     adj[u].push_back(v);
     adj[v].push_back(u);
 }
 
 int main(){
     int n=5;
-
     vector<vector<int>> adj(n);
-    addEdge(adj,0,1);
-    addEdge(adj,0,2);
-    addEdge(adj,1,3);
-    addEdge(adj,1,4);
-    addEdge(adj,2,4);
+
+    add_edge(adj,0,1);
+    add_edge(adj,0,2);
+    add_edge(adj,1,3);
+    add_edge(adj,1,4);
+    add_edge(adj,2,4);
 
     bfs(adj,0);
 
