09e6a9267067b7dbd5a401de09a6085217c3efa4
Suraya Jannat Mim <150699804+Mim1726@users.noreply.github.com>
2025-02-08 20:06:23 +0600

    Update 3_c.cpp

--- Final Prev/28th/3_c.cpp
+++ Final Prev/28th/3_c.cpp
@@ -1,23 +1,18 @@
-#include <iostream>
+#include<bits/stdc++.h>
 using namespace std;
 /*
-Design an approach to sort the nodes of a linked list containing 
+Design an approach to sort the nodes of a linked list containing
 integer values without using any external array.
 */
-struct ll
-{
-    struct node
-    {
-        node *next;
+struct ll{
+    struct node{
         int val;
+        node* next;
     };
-    node *head=NULL;
-    node *tail=NULL;
+    node* head=NULL;
+    node* tail=NULL;
 
-    void insert_last(int x)
-    {
-        //insert x at last
-        //Your code starts here #001
+    void insert_last(int x){
         node* a=(node*)malloc(sizeof(node));
         a->next=NULL;
         a->val=x;
@@ -29,66 +24,47 @@ struct ll
             head=a;
             tail=a;
         }
-        //Your code ends here #001
     }
-
     void sort(){
-        //Sorts the linked list
-
-        //Your code starts here #002
         if(head==NULL)
             return;
+
         node* p=head;
         while(p){
             node* tmp1=p;
             node* tmp2=p->next;
             while(tmp2){
-                if((tmp1->val)>(tmp2->val)){
+                if(tmp1->val > tmp2->val){
                     tmp1=tmp2;
                 }
                 tmp2=tmp2->next;
             }
-            if(tmp1!=p){
+            if(tmp1 != p){
                 int tmp=p->val;
                 p->val=tmp1->val;
                 tmp1->val=tmp;
             }
             p=p->next;
         }
-        //Your code ends here #002
     }
-
-    void print()
-    {
-        //prints elements from head to tail
-        //Your code starts here #003
+    void print(){
         node* p=head;
         while(p){
-            cout << p->val << endl;
+            cout << p->val << " ";
             p=p->next;
         }
-
-        //Your code ends here #003
     }
 };
 
-
-int main()
-{
-
-    //Your code starts here #004
+int main(){
     ll my_list;
     int n;
     cin >> n;
     for(int i=0;i<n;i++){
-        int v;
-        cin >> v;
-        my_list.insert_last(v);
+        int x;
+        cin >> x;
+        my_list.insert_last(x);
     }
     my_list.sort();
     my_list.print();
-    //Your code ends here #004
-    return 0;
-
-
 }
