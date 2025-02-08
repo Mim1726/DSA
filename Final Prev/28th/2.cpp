d4e0bb16e2aab80f898a2e054f40077fca9607b5
Suraya Jannat Mim <150699804+Mim1726@users.noreply.github.com>
2025-02-08 06:38:12 +0600

    Create 2.cpp

---
+++ Final Prev/28th/2.cpp
@@ -0,0 +1,11 @@
+Consider a disjoint set data structure considering the elements 1-10(inclusive).
+
+(a) Perform the following operations without path compression. You must
+    draw the trees in each step.
+    a.union(1,9)   b.union(2,4)   c.union(4,5)   d.union(6,3)
+    e.union(6,7)   f.union(6,8)   g.union(9,6)
+    
+(b) Perform the following operations with path compression. You must
+    draw the trees in each step.
+    a.union(1,9)   b.union(2,4)   c.union(4,5)   d.union(6,3)
+    e.union(6,7)   f.union(6,8)   g.union(9,6)
