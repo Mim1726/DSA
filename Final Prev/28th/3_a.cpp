3a2af9ad22fdf967d9c85c8e1a9ca7b6a851bfda
Suraya Jannat Mim <150699804+Mim1726@users.noreply.github.com>
2025-02-10 07:10:42 +0600

    Create 3_a.cpp

---
+++ Final Prev/28th/3_a.cpp
@@ -0,0 +1,5 @@
+Design a linked list with the following properties:
+    (a) Insertion of an item in the first or last position has the 
+        complexity of O(1)
+    (b) Deletion of an item from the middle position hax the complexity 
+        of O(1)
